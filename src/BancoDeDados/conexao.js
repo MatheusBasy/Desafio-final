f9e7e4deb096692d9d075780655a2150381ead38
Matheus Basy <141868150+MatheusBasy@users.noreply.github.com>
2023-11-28 18:07:35 -0300

    Update conexao.js

--- src/BancoDeDados/conexao.js
+++ src/BancoDeDados/conexao.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
+        host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,

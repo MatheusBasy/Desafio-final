5ab79c6a89e848a50540a2d2b8b4c454487ac325
Matheus Basy <141868150+MatheusBasy@users.noreply.github.com>
2023-11-29 11:13:10 -0300

    Update rotas.js

--- src/rotas.js
+++ src/rotas.js
@@ -1,14 +1,14 @@
+require('dotenv').config()
 const express = require('express');
+const rotas = express();
 const usuarios = require('./componentes/usuarios');
 const listar = require('./componentes/categorias');
 const tokenVerify = require('./intermediarios/auth');
-const rotas = express();
-require('dotenv').config()
 
 rotas.post('/usuario', usuarios.cadastrar);
 rotas.post('/login', usuarios.login);
 rotas.get("/categoria", listar);
-rotas.get("/usuario", tokenVerify, usuarios.detalharPerfil);
-rotas.put("/usuario", tokenVerify, usuarios.atualizarPerfil);
+rotas.get('/usuario', tokenVerify, usuarios.detalharPerfil)
+rotas.put('/usuario', tokenVerify, usuarios.atualizarPerfil)
 
 module.exports = rotas;

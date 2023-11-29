ca3e9301182fc11ff5475acb5531a8104ecaf280
Cicero Santos <cjosinaldopatricio@gmail.com>
2023-11-29 10:49:03 -0300

    adicionar rotas detalhar e atualizar perfil

--- src/rotas.js
+++ src/rotas.js
@@ -6,6 +6,10 @@ require('dotenv').config()
 
 rotas.post('/usuario', usuarios.cadastrar);
 rotas.post('/login', usuarios.login);
+
+rotas.put('/usuario', usuarios.atualizarPerfil);
+
+rotas.get('/usuario', auth, usuarios.detalharPerfil);
 rotas.get("/categoria", listar);
 
 module.exports = rotas;

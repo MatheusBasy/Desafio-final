e6bc2c4a0bf7334db6db5ff59042dbac98fbcd62
wesleysilva25 <vetwesley@hotmail.com>
2023-12-01 17:22:22 -0300

    Finalizado criação do banco de dados editar cadastrar e listar produtos

--- src/componentes/Produtos.js
+++ src/componentes/Produtos.js
@@ -1,9 +1,21 @@
 const knex = require('../BancoDeDados/conexao');
 
 const CadastrarProduto = async (req, res) => {
+
+  const usuario = req;
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+          
+
+  if (!usuario) {
+    return res.status(401).json({ messagem: 'Usuário não autenticado' });
+  }
+
+  if (!descricao || !quantidade_estoque || !valor || !categoria_id) {
+    return res.status(400).json({ messagem: 'Todos os campos são obrigatórios' });
+  }
     try {
-      const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
-  
+     
+
       await knex('produtos').insert({
         descricao,
         quantidade_estoque,
@@ -11,43 +23,57 @@ const CadastrarProduto = async (req, res) => {
         categoria_id,
       });
   
-      return res.status(200).json({ message: 'Produto cadastrado com sucesso' });
+      return res.status(200).json({ messagem: 'Produto cadastrado com sucesso' });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erro interno do servidor' });
+      res.status(500).json({ messagem: 'Erro interno do servidor' });
     }
   };
   const EditarProduto =  async (req, res) => {
+    
+    const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+    const { id } = req.params;
+
+    const usuario = req;
+  if (!usuario) {
+    return res.status(401).json({ messagem: 'Usuário não autenticado' });
+  }
+
+ if (!descricao || !quantidade_estoque || !valor || !categoria_id || !id) {
+    return res.status(400).json({ messagem: 'Todos os campos são obrigatórios' });
+  } 
     try {
-      const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
-      const { id } = req.params;
-  
-      await knex('produtos').where({ id }).update({
+           await knex('produtos').where({ id }).update({
         descricao,
         quantidade_estoque,
         valor,
         categoria_id,
       });
   
-      return res.status(200).json({ message: 'Produto atualizado com sucesso' });
+      return res.status(200).json({ messagem: 'Produto atualizado com sucesso' });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erro interno do servidor' });
+      res.status(500).json({ messagem: 'Erro interno do servidor' });
     }
   };
 
-  const ListarProdutos = async (req, res) => {
+  const ListarProduto = async (req, res) => {
     try {
-      const produtos = await knex.select('*').from('produtos');
-      return res.status(200).json(produtos);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Erro interno do servidor' });
+    const { categoria_id } = req.query;
+    let query = knex.select('*').from('produtos');
+    if (categoria_id) {
+      query = query.where({ categoria_id });
     }
-  };
+    const produtos = await query;
+    return res.status(200).json(produtos);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ messagem: 'Erro interno do servidor' });
+  }
+};
 
   module.exports = {
     CadastrarProduto,
     EditarProduto,
-    ListarProdutos
+    ListarProduto
 };
--- src/rotas.js
+++ src/rotas.js
@@ -4,11 +4,16 @@ const rotas = express();
 const usuarios = require('./componentes/usuarios');
 const listar = require('./componentes/categorias');
 const tokenVerify = require('./intermediarios/auth');
+const { CadastrarProduto, EditarProduto, ListarProduto } = require('./componentes/Produtos');
 
 rotas.post('/usuario', usuarios.cadastrar);
 rotas.post('/login', usuarios.login);
 rotas.get("/categoria", listar);
-rotas.get('/usuario', tokenVerify, usuarios.detalharPerfil)
-rotas.put('/usuario', tokenVerify, usuarios.atualizarPerfil)
+rotas.get('/usuario', tokenVerify, usuarios.detalharPerfil);
+rotas.put('/usuario', tokenVerify, usuarios.atualizarPerfil);
+rotas.post('/produto', tokenVerify , CadastrarProduto);
+rotas.put('/produto/:id', tokenVerify , EditarProduto);
+rotas.get('/produto', tokenVerify , ListarProduto);
+
 
 module.exports = rotas;

0b7399407f6c32a6b44a1092dcd676d882dbe023
Cicero Santos <cjosinaldopatricio@gmail.com>
2023-12-04 19:11:34 -0300

    corrigindo conflitos

--- src/rotas.js
+++ src/rotas.js
@@ -5,6 +5,7 @@ const usuarios = require('./componentes/usuarios');
 const clientes = require('./componentes/clientes');
 const listar = require('./componentes/categorias');
 const tokenVerify = require('./intermediarios/auth');
+const { CadastrarProduto, EditarProduto, ListarProduto } = require('./componentes/Produtos');
 
 rotas.post('/usuario', usuarios.cadastrar);
 rotas.post('/login', usuarios.login);
@@ -15,5 +16,9 @@ rotas.post('/cliente', tokenVerify, clientes.cadastrar)
 rotas.put('/cliente/:id', tokenVerify, clientes.editar)
 rotas.get('/cliente', tokenVerify, clientes.listar)
 rotas.get('/cliente/:id', tokenVerify, clientes.detalhar)
+rotas.post('/produto', tokenVerify , CadastrarProduto);
+rotas.put('/produto/:id', tokenVerify , EditarProduto);
+rotas.get('/produto', tokenVerify , ListarProduto);
+
 
 module.exports = rotas;
